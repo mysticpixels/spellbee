41019ef9fac856ad11eac078e018de659417fbaf
Ranjith kumar <mysticpixels@gmail.com>
2015-12-09 19:04:41 +0530

    Initial commit - Basic working JS, Markup, CSS & Assets added

---
+++ js/ikit.js
@@ -0,0 +1,170 @@
+/*=========================================================================
+=            THe behaviour layer of 'Spell the object' game            =
+==========================================================================*/
+
+// the game object
+var theGame = {
+
+	// array of url and name of the pic to match
+	names : [ 'diya', 'rocket', 'monkey', 'apple', 'orange', 'car', 'school', 'police', 'tree', 'cow'],
+
+	// task status number
+	status : 0,
+
+	// keystroke order array
+	keyStrokeOrder : [],
+
+	/*----------  DOM object initialisations  ----------*/
+
+	// pic which is to be spelled
+	pic : document.getElementById("objectToSpell").querySelectorAll('img')[0],
+
+	// keyboard
+	keyBoard : document.getElementById("keyBoard"),
+
+	// reset link	
+	resetLink : document.getElementById("reset"),
+
+	// next link	
+	nextLink : document.getElementById("next"),
+
+	// keyedin string display header
+	keyedStringDisplay : document.getElementById("keyedStringDisplay"),
+
+	// the ul element of keyboard
+	keyBoardUL : document.getElementById("keyBoardUL"),
+
+	/*----------  methods  ----------*/
+
+	// method to reset the keyboard
+	reset : function(){
+
+		//  removing classname 'active' from all li tags of keyboard
+		for(var i=0;i<keyBoard.querySelectorAll('a').length;i++){
+			keyBoard.querySelectorAll('a')[i].parentNode.className="";
+		}
+		theGame.keyedStringDisplay.innerHTML="";
+	},
+
+	// next image detail loaded
+	next : function(){
+		if(theGame.status<theGame.names.length-1){
+			var imgSrc="images/"+theGame.names[theGame.status+1]+".jpg";
+			theGame.status++;
+			theGame.pic.setAttribute('src', imgSrc);	
+			theGame.clearKeyboard();
+			theGame.clearDisplay();
+			theGame.populateKeyBoard();
+		}
+		return;
+	},
+
+	// populating the keyboard keys with random alphabets
+	// todo: add the first image also on first load ... rt now its hard coded in the markup
+	populateKeyBoard : function(e){
+		var i, j, k, temp, ambiguity,
+		alphabetList=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+		nameWord=[],
+		randomer=[]; // array which will hold all the unique random numbers for placing the alphabets in the keyboard
+
+		// generating 15 keys in the form of UL and LI and anchortags
+		for (i=0; i<15/*(theGame.names[theGame.status].length+5)*/	; i++) {
+			var listItem = document.createElement('li');
+			var anchorItem = document.createElement('a');
+
+			// attaching eventlisteners while the elements are created
+			anchorItem.addEventListener('click', theGame.keyIn, 'false');
+			anchorItem.setAttribute('href', '#');
+			anchorItem.innerHTML=alphabetList[Math.round(Math.random()*25)];
+			keyBoardUL.appendChild(listItem);
+			listItem.appendChild(anchorItem);
+		};
+
+		// creating an array with individual alphabets of each names so that it can be randomly placed in the keyboard
+		for (j = 0; j < theGame.names[theGame.status].length; j++) {
+			nameWord.push(theGame.names[theGame.status][j]);
+		};
+
+		// generating unique random numbers, and saving those in an array 'randomer'
+		while ((randomer.length)<(nameWord.length)) {
+			temp=Math.round(Math.random()*14); //generating a random number <= the total number of keys in the board
+			ambiguity=false; // setting a flag variable which checks for repeating random numbers
+			for(k=0; k<nameWord.length; k++){ // looping through all the elements in the array, which will store the unique random numbers
+				if(randomer[k]==temp){ // checking if any of the array item is the same as the random number generated above
+					ambiguity=true; // if yes, the flag is set to true
+					break; // and the loop is broken
+				}
+			}
+			if(!ambiguity){ // only if the flag value is false, 
+				randomer.push(temp); // the unique random number generated is pushed to the array of unique random numbers
+			}
+		}
+
+		// placing alphabet-wise in each of the anchor tags, with the generated random numbers as their position in the child DOM order
+		for(k=0; k<randomer.length; k++){
+			keyBoardUL.children[randomer[k]].children[0].innerHTML=nameWord[k];
+		}
+	},
+
+	// clear all keyboard markup
+	clearKeyboard : function(e){
+
+		// storing the total number of keys in the current keyboard
+		var keyCount = theGame.keyBoardUL.children.length,
+		i;
+
+		// removing all the key DOM elements in the keyboard
+		for (i = keyCount-1; i >= 0; i--) {
+			theGame.keyBoardUL.children[i].parentNode.removeChild(theGame.keyBoardUL.children[i]);//todo: not necessary to remove li DOM elements, just remove the className
+		}
+	},
+
+	//clearing the header display of keyed in alphabets
+	clearDisplay : function(e){
+		theGame.keyedStringDisplay.innerHTML="";
+	},
+
+	// all the action that happens while the keys are pressed in the keyboard
+	keyIn : function(e){
+		var eventName=e;
+
+		// recording the key press order in the array object keyStrokeOrder
+		theGame.keyStrokeOrder.push(e.currentTarget.parentNode.id);
+		// console.log('pushed');
+		
+		// in case the key is a fresh press
+		if(e.currentTarget.parentNode.className!='active'){
+			e.currentTarget.parentNode.className+='active';
+			theGame.keyedStringDisplay.innerHTML+=e.currentTarget.text;
+		}
+	},
+
+	// the start of all fun ;)
+	startUp : function(e){
+
+		//loading the picture and setting the count
+		theGame.status=0; //todo: no need to set it at the first load
+
+		// populate the image
+		theGame.populateImage();
+
+		// populating the keyboard
+		theGame.populateKeyBoard();
+
+		// attaching the click event to each keys in the keyboard
+		for(var i=0;i<keyBoard.querySelectorAll('a').length;i++){
+			keyBoard.querySelectorAll('a')[i].addEventListener('click', theGame.keyIn, 'false');
+		}
+
+		// resetting the keyboard
+		theGame.resetLink.addEventListener('click', theGame.reset, 'false');
+
+		//attaching listener to next link
+		next.addEventListener('click', theGame.next, 'false');
+	}
+}
+
+// onload triggers
+window.onload = function(){
+	theGame.startUp();
+};
